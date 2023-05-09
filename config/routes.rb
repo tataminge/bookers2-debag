e7b20a6e8ce890a14ca4c615ef852254c7056e67
EC2 Default User <ec2-user@ip-172-31-47-223.ap-northeast-1.compute.internal>
2023-05-09 05:28:25 +0000

    応用課題5

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,18 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "/home/about"=>"homes#about"
+  get "search" => "searches#sear"
 
-# 親子関係にする.いいねはbookにくっついている
+  # 親子関係にする.いいねはbookにくっついている
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  # memberを使うことで別のルーティングを追加できる
+
   resources :users, only: [:index,:show,:edit,:update] do
-    member do
-      get :follows, :followers
-    end
     resource :relationships, only: [:create, :destroy]
+    	get 'followings' => 'relationships#followings', as: 'followings'
+  	  get 'followers' => 'relationships#followers', as: 'followers'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
